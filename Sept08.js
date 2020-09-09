ae065b5d3a5ec0e17fbb6eebe4609817a37cd8e6
ccedacero <cristian.cedacero@gmail.com>
2020-09-08 21:04:09 -0400

    add recurseCallback & flatten

--- Sept08.js
+++ Sept08.js
@@ -1,8 +1,57 @@
-function reverse(str) {
-  if (str.length <= 1) {
-    return str;
+// function reverse(str) {
+//   if (str.length <= 1) {
+//     return str;
+//   }
+//   console.log(str);
+//   return reverse(str.slice(1)) + str[0];
+// }
+// console.log(reverse("hello"));
+
+function isPalindrome(str) {
+  if (str.length === 1) {
+    return true;
   }
-  console.log(str);
-  return reverse(str.slice(1)) + str[0];
+  if (str.length === 2) {
+    return str[0] === str[1];
+  }
+  if (str[0] === str.slice(-1)) {
+    // console.log(str.slice(1, -1));
+    return isPalindrome(str.slice(1, -1));
+  }
+  return false;
 }
-console.log(reverse("hello"));
+// console.log(isPalindrome("awesome")); // false
+// console.log(isPalindrome("foobar")); // false
+// console.log(isPalindrome("amanaplanacanalpanama")); // true
+// console.log(isPalindrome("amanaplanacanalpandemonium")); // false
+// console.log(isPalindrome("tacocat")); // true
+
+// function someRecursive(arr, callback) {
+//   if (arr.length === 0) return false;
+//   let val = arr.pop();
+
+//   if (callback(val) === true) {
+//     return true;
+//   } else {
+//     return someRecursive(arr, callback);
+//   }
+// }
+
+// const isOdd = (val) => val % 2 !== 0;
+// console.log(someRecursive([1,2], isOdd)); // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function flatten(arr) {
+  let flattenArr = [];
+  arr.forEach((val) => {
+    if (Array.isArray(val)) {
+      flattenArr = flattenArr.concat(flatten(val));
+    } else {
+      flattenArr.push(val);
+    }
+  });
+  return flattenArr;
+}
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
