9f7f4ebbaf7ad46bc81df875a8d2a2e18ff7cb26
ccedacero <cristian.cedacero@gmail.com>
2020-09-09 20:51:09 -0400

    add binary search refactor

--- Sept09-BinarySearch.js
+++ Sept09-BinarySearch.js
@@ -2,14 +2,9 @@ function binarySearch(arr, val) {
   let start = 0;
   let end = arr.length - 1;
   let middle = Math.floor((start + end) / 2);
-
   while (arr[middle] !== val && start <= end) {
-    if (val < arr[middle]) {
-      end = middle - 1;
-    }
-    if (val > arr[middle]) {
-      start = middle + 1;
-    }
+    if (val < arr[middle]) end = middle - 1;
+    if (val > arr[middle]) start = middle + 1;
     middle = Math.floor((start + end) / 2);
   }
   if (arr[middle] === val) {
@@ -17,4 +12,4 @@ function binarySearch(arr, val) {
   }
   return -1;
 }
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13));
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28));
