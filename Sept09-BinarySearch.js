ca035b4e4f411385d107d52f7961ae9756f9fb21
ccedacero <cristian.cedacero@gmail.com>
2020-09-09 21:47:58 -0400

    add findSubstring

--- Sept09-BinarySearch.js
+++ Sept09-BinarySearch.js
@@ -13,3 +13,20 @@ function binarySearch(arr, val) {
   return -1;
 }
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28));
+
+function findSubstring(longStr, shortStr) {
+  let count = 0;
+  for (let i = 0; i < longStr.length; i++) {
+    for (j = 0; j < shortStr.length; j++) {
+      if (shortStr[j] !== longStr[i + j]) {
+        break;
+      }
+      if (j === shortStr.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(findSubstring("lorielol loledlol", "l"));
