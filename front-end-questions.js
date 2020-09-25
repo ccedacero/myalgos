67103d7aba41eead2774d10719013da826b8c84f
ccedacero <cristian.cedacero@gmail.com>
2020-09-24 22:16:28 -0400

    add reverse str functions & general interview q

--- front-end-questions.js
+++ front-end-questions.js
@@ -1,1 +1,17 @@
+// Question: What is the value of foo?
+
 var foo = 10 + "20";
+// foo contacts 10 + 20 = '1020'
+
+// Question: What will be the output of the code below?
+console.log(0.1 + 0.2 == 0.3);
+// false
+
+// Question: What value is returned from the following statement?
+"i'm a lasagna hog".split("").reverse().join("");
+// "goh angasal a m'i"
+
+// Question: What is the value of window.foo?
+
+window.foo || (window.foo = "bar");
+// "1020"
